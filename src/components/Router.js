f7749b2259b1c67ed158ec2c6d9abcfc6e8e70ea
Mansi Singhai <mansisinghai@192.168.1.19>
2023-10-20 17:11:03 +0530

    added forgot-password component

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Forms/Login";
 import Register from "./Forms/Register";
+import ForgotPassword from "./Forms/ForgotPassword";
+import ResetPassword from "./Forms/ResetPassword";
 
 const Router = () => {
 	return (
@@ -9,6 +11,8 @@ const Router = () => {
 			<Routes>
 				<Route path="/login" element={<Login />} />
 				<Route path="/" element={<Register />} />
+				<Route path="/reset-password" element={<ResetPassword />} />
+				<Route path="/forgot-password" element={<ForgotPassword/>}/>
 			</Routes>
 		</BrowserRouter>
 	);
